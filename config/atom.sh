b63b5be8b907971744c153da4b9ec588c2bdd39a
sdavidson140 <sdavidson140@gmail.com>
2017-04-27 12:13:55 -0400

    confirm apm is available before atom script runs

--- config/atom.sh
+++ config/atom.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-apm install aligner aligner-scss aligner-css aligner-ruby atom-beautify \
-editorconfig linter-ui-default busy-signal esformatter fixmyjs git-diff-details git-history \
-git-plus language-markdown less-than-slash linter linter-csslint linter-eslint \
-linter-markdown linter-rubocop linter-ruby linter-scss-lint linter-tidy \
-markdown-writer sort-lines language-ember-htmlbars intentions linter-sass-lint \
-standardjs-snippets
+if which apm; then
+  apm install aligner aligner-scss aligner-css aligner-ruby atom-beautify \
+  editorconfig linter-ui-default busy-signal esformatter fixmyjs git-diff-details git-history \
+  git-plus language-markdown less-than-slash linter linter-csslint linter-eslint \
+  linter-markdown linter-rubocop linter-ruby linter-scss-lint linter-tidy \
+  markdown-writer sort-lines language-ember-htmlbars intentions linter-sass-lint \
+  standardjs-snippets
 
-apm disable autocomplete-snippets
+  apm disable autocomplete-snippets
 
-apm enable autosave
+  apm enable autosave
+else
+  echo -e "\n There was an issue with atom installation. Please ask for assistance. \n"
+fi

13e137e0d117b7e3de4937b1937b16753353556b
sdavidson140 <sdavidson140@gmail.com>
2017-06-08 17:14:53 -0400

    Refactor nvm script, test for Linux

--- config/nvm.sh
+++ config/nvm.sh
@@ -1,30 +1,19 @@
 #!/usr/bin/env bash
 if [[ $# -eq 0 ]] ; then
   brew install nvm
-  if [[ $(uname -s) != 'Darwin' ]]; then
-    #LINUX
-    mkdir -p ~/home/$(whoami)/.nvm
+  mkdir -p ~/.nvm
+
 cat <<'EOF' >> ~/.bashrc
 
-  export NVM_DIR="/home/$(whoami)/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR=~/.nvm
+. $(brew --prefix nvm)/nvm.sh
 
 EOF
 
-  else
-    #MAC
-    mkdir -p ~/.nvm
-cat <<'EOF' >> ~/.bashrc
-
-  export NVM_DIR=~/.nvm
-  . $(brew --prefix nvm)/nvm.sh
-
-EOF
-  fi
 fi
 
 if [[ $1 = "node" ]]; then
-  . "$NVM_DIR/nvm.sh" || . $(brew --prefix nvm)/nvm.sh
+  . $(brew --prefix nvm)/nvm.sh
   nvm install --lts=boron || nvm install 6.9.4
   nvm alias default v6
   nvm use default
